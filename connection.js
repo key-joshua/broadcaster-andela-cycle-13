b13ea5b647e66c145ddc35bf05e93875b72a4ee9
key_joshua <k.joshua855@gmail.com>
2019-11-16 04:26:13 +0200

    done-with-connection-file
    ch(done-with-connection-file):As developer i have built connection of app[finishes #169745853]

---
+++ connection.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import apiDocumentation from './swagger.json';
+import authapp from './server/v1/api/routes/Router';
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const basePath = '/api/v1';
+const docUrl = 'localhost:1000/api/v1/documentation/';
+
+app.use(basePath, authapp);
+app.use(`${basePath}/documentation`, swaggerUi.serve, swaggerUi.setup(apiDocumentation));
+app.listen(process.env.PORT || 1000, () => { console.log('server is running on port 1000'); });
+app.get('**', (req, res) => {
+  res.status(200).send({ status: 200,
+    message: 'Hey !! You are Welcome to API version 1 of Broadcaster, Now Server Connetion is Live, go with this link below it all about documentation of this API',
+    link: docUrl,
+  });
+});
+
+export default app;
