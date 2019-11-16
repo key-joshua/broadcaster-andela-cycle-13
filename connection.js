c35ece011518bf97c04212c20d294b3c8a185f7f
key_joshua <k.joshua855@gmail.com>
2019-11-16 14:38:13 +0200

    changing app running port

--- connection.js
+++ connection.js
@@ -7,11 +7,11 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const basePath = '/api/v1';
-const docUrl = 'localhost:1000/api/v1/documentation/';
+const docUrl = 'localhost:3000/api/v1/documentation/';
 
 app.use(basePath, authapp);
 app.use(`${basePath}/documentation`, swaggerUi.serve, swaggerUi.setup(apiDocumentation));
-app.listen(process.env.PORT || 1000, () => { console.log('server is running on port 1000'); });
+app.listen(process.env.PORT || 3000, () => { console.log('server is running on port 3000'); });
 app.get('**', (req, res) => {
   res.status(200).send({ status: 200,
     message: 'Hey !! You are Welcome to API version 1 of Broadcaster, Now Server Connetion is Live, go with this link below it all about documentation of this API',
