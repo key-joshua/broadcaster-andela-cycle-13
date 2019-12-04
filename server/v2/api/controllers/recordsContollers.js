54a1bb77d5cebf64a0513c3a65ba4b196afe4567
key_joshua <k.joshua855@gmail.com>
2019-12-04 12:28:11 +0200

    done with create record endpoint
    ft(done-with-create-record-endpoint):As developer i have built create record endpoint with database of app[finishes #170095871]

---
+++ server/v2/api/controllers/recordsContollers.js
@@ -0,0 +1,53 @@
+import nodemailer from 'nodemailer';
+import impData from '../models/DB';
+import imphelp from '../helpers/createHelper';
+
+class Records {
+  async createRecord(req, res) {
+    const decUserDetail = req.attachedWithInfo;
+    const { createdBy, title, type, latitude, longitude, comment } = req.body;
+    const createValidate = imphelp.schemaCreate(req.body);
+    if (createValidate.error) {
+      const mess = createValidate.error.details[0].message;
+      const messUser = mess.replace(/\"/g, '');
+      res.status(400).send({ status: 400, message: messUser });
+    }
+    if (!createValidate.error) {
+      let imgs;
+      let vids;
+      if (req.files) {
+        if (req.files.images) {
+          const imgsPath = req.files.images.map(({ path }) => path);
+          imgs = imgsPath.join(',  ');
+        }
+        if (req.files.videos) {
+          const vidsPath = req.files.videos.map(({ path }) => path);
+          vids = vidsPath.join(',  ');
+        }
+      }
+      const chekType = imphelp.checkType(type);
+      if (chekType) {
+        return res.status(400).json({
+          status: 400,
+          message: `Hey ${decUserDetail.username} record type should be red-flags or intervetion` });
+      }
+      const readyDatas = {
+        userId: decUserDetail.id,
+        createdBy: createdBy || decUserDetail.username,
+        title: title,
+        type: type,
+        latitude: latitude || 'none',
+        longitude: longitude || 'none',
+        images: imgs || 'none',
+        videos: vids || 'none',
+        comment: comment,
+      };
+      const createdRecord = await impData.createRecord(readyDatas);
+      const data = await impData.fetchOneRecord(createdRecord[0].recordid);
+      res.status(201).json({ status: 201, message: `Hy ${decUserDetail.username} your record has created successfully on ${imphelp.created}`, data: data });
+    }
+  }
+
+}
+const expRecords = new Records();
+export default expRecords;
---
+++ test/vb/bCreateRecordsTest.js
@@ -0,0 +1,232 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../connection';
+import impDB from './allTestDB';
+import imptokelp from '../../server/v2/api/helpers/tokenHelper';
+
+chai.use(chaiHttp);
+const router = () => chai.request(app);
+describe('my Testing suite', () => {
+  const userToken = imptokelp.userCreatedToken;
+  const adminToken = imptokelp.adminCreatedToken;
+
+
+  it('users should not be able to create record when did not insert title of record', (done) => {
+    router()
+      .post('/api/v2/red-flags/')
+      .set('Authorization', userToken)
+      .send(impDB[18])
+      .end((error, response) => {
+        expect(response).to.have.status([400]);
+        expect(response.body).to.be.a('object');
+        expect(response.body).to.have.property('status');
+        expect(response.body.status).to.be.equal(400);
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.be.a('string');
+        done(error);
+      });
+  });
+
+  it('users should not be able to create record when did not insert type of record', (done) => {
+    router()
+      .post('/api/v2/red-flags/')
+      .set('Authorization', userToken)
+      .send(impDB[19])
+      .end((error, response) => {
+        expect(response).to.have.status([400]);
+        expect(response.body).to.be.a('object');
+        expect(response.body).to.have.property('status');
+        expect(response.body.status).to.be.equal(400);
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.be.a('string');
+        done(error);
+      });
+  });
+
+  it('users should not be able to create record when did not insert comment on record', (done) => {
+    router()
+      .post('/api/v2/red-flags/')
+      .set('Authorization', userToken)
+      .send(impDB[20])
+      .end((error, response) => {
+        expect(response).to.have.status([400]);
+        expect(response.body).to.be.a('object');
+        expect(response.body).to.have.property('status');
+        expect(response.body.status).to.be.equal(400);
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.be.a('string');
+        done(error);
+      });
+  });
+
+  it('users should be able to create record used wrong record type', (done) => {
+    router()
+      .post('/api/v2/red-flags/')
+      .set('Authorization', userToken)
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .field('title', 'Bad road')
+      .field('type', 'redflags')
+      .field('comment', 'testo')
+      .attach('images', 'server/v1/api/models/uploadedFiles/imga.png')
+      .attach('videos', 'server/v1/api/models/uploadedFiles/sample.mp4')
+      .end((error, response) => {
+        expect(response).to.have.status([400]);
+        expect(response.body).to.be.a('object');
+        expect(response.body).to.have.property('status');
+        expect(response.body.status).to.be.equal(400);
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.be.a('string');
+        done(error);
+      });
+  });
+
+  it('users should be able to create record when provide token', (done) => {
+    router()
+      .post('/api/v2/red-flags/')
+      .set('Authorization', userToken)
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .field('title', 'obused girls')
+      .field('type', 'redflag')
+      .field('comment', 'test')
+      .attach('images')
+      .attach('videos')
+      .end((error, response) => {
+        expect(response).to.have.status([201]);
+        expect(response.body).to.be.a('object');
+        expect(response.body).to.have.property('status');
+        expect(response.body.status).to.be.equal(201);
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.be.a('string');
+        expect(response.body).to.have.property('data');
+        done(error);
+      });
+  });
+
+  it('users should be able to create record with images and videos when provide token', (done) => {
+    router()
+      .post('/api/v2/red-flags/')
+      .set('Authorization', userToken)
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .field('title', 'Bad road')
+      .field('type', 'intervetion')
+      .field('comment', 'testo')
+      .attach('images', 'server/v2/api/models/uploadedFiles/hungry.png')
+      .attach('videos', 'server/v2/api/models/uploadedFiles/sample.mp4')
+      .end((error, response) => {
+        expect(response).to.have.status([201]);
+        expect(response.body.status).to.be.equal(201);
+        expect(response.body).to.be.a('object');
+        expect(response.body).to.have.property('status');
+        expect(response.body.status).to.be.equal(201);
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.be.a('string');
+        expect(response.body).to.have.property('data');
+        done(error);
+      });
+  });
+
+  it('users should be able to create record with images and videos when provide token', (done) => {
+    router()
+      .post('/api/v2/red-flags/')
+      .set('Authorization', userToken)
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .field('title', 'obused girls')
+      .field('type', 'redflag')
+      .field('comment', 'test')
+      .attach('images', 'server/v2/api/models/uploadedFiles/hungry.png')
+      .attach('videos', 'server/v2/api/models/uploadedFiles/sample.mp4')
+      .end((error, response) => {
+        expect(response).to.have.status([201]);
+        expect(response.body).to.be.a('object');
+        expect(response.body).to.have.property('status');
+        expect(response.body.status).to.be.equal(201);
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.be.a('string');
+        expect(response.body).to.have.property('data');
+        done(error);
+      });
+  });
+
+  it('users should be able to create record with images and videos when provide token', (done) => {
+    router()
+      .post('/api/v2/red-flags/')
+      .set('Authorization', userToken)
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .field('title', 'obused girls')
+      .field('type', 'redflag')
+      .field('comment', 'test')
+      .attach('images', 'server/v2/api/models/uploadedFiles/hungry.png')
+      .attach('videos', 'server/v2/api/models/uploadedFiles/sample.mp4')
+      .end((error, response) => {
+        expect(response).to.have.status([201]);
+        expect(response.body).to.be.a('object');
+        expect(response.body).to.have.property('status');
+        expect(response.body.status).to.be.equal(201);
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.be.a('string');
+        expect(response.body).to.have.property('data');
+        done(error);
+      });
+  });
+
+  it('users should be able to create record with images and videos when provide token', (done) => {
+    router()
+      .post('/api/v2/red-flags/')
+      .set('Authorization', userToken)
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .field('title', 'obused girls')
+      .field('type', 'redflag')
+      .field('comment', 'test')
+      .attach('images', 'server/v2/api/models/uploadedFiles/hungry.png')
+      .attach('videos', 'server/v2/api/models/uploadedFiles/sample.mp4')
+      .end((error, response) => {
+        expect(response).to.have.status([201]);
+        expect(response.body).to.be.a('object');
+        expect(response.body).to.have.property('status');
+        expect(response.body.status).to.be.equal(201);
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.be.a('string');
+        expect(response.body).to.have.property('data');
+        done(error);
+      });
+  });
+
+  it('users should be able to create record with images and videos when provide token', (done) => {
+    router()
+      .post('/api/v2/red-flags/')
+      .set('Authorization', userToken)
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .field('title', 'obused girls')
+      .field('type', 'redflag')
+      .field('comment', 'test')
+      .attach('images', 'server/v2/api/models/uploadedFiles/hungry.png')
+      .attach('videos', 'server/v2/api/models/uploadedFiles/sample.mp4')
+      .end((error, response) => {
+        expect(response).to.have.status([201]);
+        expect(response.body).to.be.a('object');
+        expect(response.body).to.have.property('status');
+        expect(response.body.status).to.be.equal(201);
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.be.a('string');
+        expect(response.body).to.have.property('data');
+        done(error);
+      });
+  });
+
+  it('users should be able to create record when did not have images or videos of record', (done) => {
+    router()
+      .post('/api/v2/red-flags/')
+      .set('Authorization', userToken)
+      .send(impDB[21])
+      .end((error, response) => {
+        expect(response).to.have.status([201]);
+        expect(response.body).to.be.a('object');
+        expect(response.body).to.have.property('status');
+        expect(response.body.status).to.be.equal(201);
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.be.a('string');
+        expect(response.body).to.have.property('data');
+        done(error);
+      });
+  });
+});
