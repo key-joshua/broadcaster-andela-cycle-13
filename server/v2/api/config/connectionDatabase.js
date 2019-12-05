ece186843aebb71ab8f6bc4ff90d662c95180c6f
key_joshua <k.joshua855@gmail.com>
2019-12-06 00:12:53 +0200

    fixed connection

--- server/v2/api/config/connectionDatabase.js
+++ server/v2/api/config/connectionDatabase.js
@@ -3,13 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const databaseURL = 'postgresql://postgres:key07202020@localhost:5432/broadcaster';
-let pool;
-if (process.env.NODE_ENV === 'production') {
-  pool = new Pool({ connectionString: process.env.DATABASE_URL });
-} else {
-  pool = new pg.Pool({
-    connectionString: databaseURL,
-  });
-}
+const pool = new pg.Pool({
+  connectionString: databaseURL,
+});
 
 export default pool;
